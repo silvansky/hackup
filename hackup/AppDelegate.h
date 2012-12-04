8f6659ee62bfe5838e198cc52832de7d74c67ee3
silvansky <v.silvansky@gmail.com>
2012-12-04 13:41:01 +0400

    Initial Commit

---
+++ hackup/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  hackup
+//
+//  Created by Valentine Gorshkov on 04.12.12.
+//  Copyright (c) 2012 silvansky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
